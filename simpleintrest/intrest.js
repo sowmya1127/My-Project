59c3898a0d787d5d090225533862ce7e94570378
sowmya1127 <77708438+sowmya1127@users.noreply.github.com>
2021-04-20 12:00:49 +0530

    Add files via upload

--- simpleintrest/intrest.js
+++ simpleintrest/intrest.js
@@ -4,15 +4,14 @@ var rate ;
 var intrest  ;
 
 function simpleintrest(){
-      principle=parseInt(document.getElementById('principle').value);
-    time=parseIntdocument.getElementById('time').value;)
-    rate=parseInt(document.getElementById('rate').value;)
-     intrest=principle*time*rate/100;
-    document.getElementById('result').value=intrest;
-     console.log('simpleintrest is'+ intrest);
+     intrest=(principle*time*rate)/100
+    document.getElementById('result').innerHTML = 'simpleintrest is ';
     
-   
+    principle=document.getElementById('principle').value;
+    time=document.getElementById('time').value;
+    rate=document.getElementById('rate').value;
+    intrest=document.getElementById('intrest').value;
     return false; 
 }
 
-
+simpleintrest();
