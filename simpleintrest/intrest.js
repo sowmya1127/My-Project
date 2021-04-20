e32580ae30597cb64830fe800791aa3d7ef66c4b
sowmya1127 <77708438+sowmya1127@users.noreply.github.com>
2021-04-20 07:19:37 +0530

    Update intrest.js

--- simpleintrest/intrest.js
+++ simpleintrest/intrest.js
@@ -7,7 +7,7 @@ function simpleintrest(){
       principle=document.getElementById('principle').value;
     time=document.getElementById('time').value;
     rate=document.getElementById('rate').value;
-     intrest=(principle*time*rate)/100;
+     intrest=principle*time*rate/100;
     document.getElementById('result').value=int;
      console.log('simpleintrest is'+ intrest);
     
