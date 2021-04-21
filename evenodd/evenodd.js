1fc9b703e3105a8fcc3f63d2abf5f0f248f0c59f
sowmya1127 <77708438+sowmya1127@users.noreply.github.com>
2021-04-21 08:18:08 +0530

    Add files via upload

--- evenodd/evenodd.js
+++ evenodd/evenodd.js
@@ -4,22 +4,32 @@ var upper;
 var even;
 var odd;
 function even(){
-    lower=parseInt(document.getElementById('lower').value.number);
-    upper=parseInt(document.getElementById('upper').value.number);
-    if(number%2==0){
-        document.getElementById('result').value=number;
-        console.log('number'+number);
+    lower=parseInt(document.getElementById('lw').value);
+    upper=parseInt(document.getElementById('up').value);
+
+    for (let number = lower; number < upper; number++) {
+        if(number%2==0){
+            document.getElementById('result').value=number;
+            console.log('number'+number);
+        
+    }
+    
         
     }
     return false
 
 }
 function odd(){
-    lower=parseInt(document.getElementById('lower').value.number);
-    upper=parseInt(document.getElementById('upper').value.number);
-    if(number%2!=0){
-        document.getElementById('result').value=number;
-        console.log('number'+number);
+    lower=parseInt(document.getElementById('lw').value);
+    upper=parseInt(document.getElementById('up').value);
+    for (let number = lower; number < upper; number++) {
+        if(number%2!=0){
+            document.getElementById('result').value=number;
+            console.log('number'+number);
+
+    }
+    
+    
     }
     return false;
 
