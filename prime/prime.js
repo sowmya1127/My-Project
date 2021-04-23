708fd305535992ad55ead8bffebc53e95f88f08e
sowmya1127 <77708438+sowmya1127@users.noreply.github.com>
2021-04-23 13:58:04 +0530

    Add files via upload

--- prime/prime.js
+++ prime/prime.js
@@ -4,10 +4,16 @@ function primeNum(){
     prime=parseInt(document.getElementById('num').value);
     for( var int=2;int<=prime/2;int++){
         if(prime%int!=0){
-            document.getElementById("result").innerHTML='It is a primeNumber';
+            output='It is a primeNumber'
+            document.getElementById("result").value=output;
             console.log('It is a primeNumber');
           
             
+        }else{
+            output='It is   not primeNumber'
+            document.getElementById("result").value=output;
+            console.log('It is not primeNumber');
+
         }
     }
 return false;
